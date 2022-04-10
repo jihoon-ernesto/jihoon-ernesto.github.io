d505fc6292c7a2c0d183cf01cafbb0d45937c38d
jihoon-ernesto <41365514+jihoon-ernesto@users.noreply.github.com>
2022-04-11 00:29:40 +0900

    Use gatsby-plugin-draft to hide a sample post
    - hide 'hello-world' post
    - ref) https://www.gatsbyjs.com/plugins/gatsby-plugin-draft/

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,6 +12,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     `
       {
         allMarkdownRemark(
+          filter: { fields: { draft: { eq: false } } }
           sort: { fields: [frontmatter___date], order: ASC }
           limit: 1000
         ) {
