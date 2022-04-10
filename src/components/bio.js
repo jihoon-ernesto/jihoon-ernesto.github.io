57c2c6203cae182513be49eecaf6a85007433b2a
jihoon-ernesto <41365514+jihoon-ernesto@users.noreply.github.com>
2022-04-10 09:51:46 +0900

    Modify bio and profile picture
    - TODO: dynamically link the profile picture to the github account

--- src/components/bio.js
+++ src/components/bio.js
@@ -19,7 +19,7 @@ const Bio = () => {
             summary
           }
           social {
-            twitter
+            github
           }
         }
       }
@@ -46,9 +46,7 @@ const Bio = () => {
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
           {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
-          </a>
+          GitHub: <a href={`https://github.com/${social?.github || ``}`}>{social?.github}</a>
         </p>
       )}
     </div>
